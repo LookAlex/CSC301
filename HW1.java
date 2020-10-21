6159f659d2b2dc6e1617b8193117dd01d9013f2a
LookAlex <59550343+LookAlex@users.noreply.github.com>
2020-10-21 00:40:26 -0500

    Add files via upload

---
+++ HW1.java
@@ -0,0 +1,33 @@
+package hw1;
+
+import java.util.TreeMap;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class HW1 {
+	/**
+	 * Inverts an array of Strings by returning a TreeMap that maps Strings to the smallest index in the array that contains the String.
+	 * 
+	 * @param a an array of Strings
+	 * @return a Map that given a String returns the smallest index of the array that contains the String
+	 * (or null if String is not in the array a).
+	 */
+	public static TreeMap<String, Integer> invert(String[] a) {
+		// TODO
+		StdOut.print("Test");
+		return null;
+	}
+	
+	/**
+	 * Computes the total number of occurrences of every String in an array.
+	 * 
+	 * @param a an array of Strings
+	 * @return a Map that given a String returns the number of occurrences of that String in the array
+	 * (or null if String is not in the array a).
+	 */
+	public static  TreeMap<String, Integer> count(String[] a) {
+		// TODO
+		StdOut.print("Test");
+		return null;
+	}
+}
---
+++ Solver.java
@@ -0,0 +1,19 @@
+package hw7;
+
+import java.util.Iterator;
+import edu.princeton.cs.algs4.Graph;
+import edu.princeton.cs.algs4.BreadthFirstPaths;
+
+public class Solver {
+	public static String solve(char[][] grid) {
+		// TODO
+		/*
+		 * 1. Construct a graph using grid
+		 * 2. Use BFS to find shortest path from start to finish
+		 * 3. Return the sequence of moves to get from start to finish
+		 */
+		
+		// Hardcoded solution to toyTest
+		return "RRRDDDDDDDDDRRRRRR";
+	}
+}
